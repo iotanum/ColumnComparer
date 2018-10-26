a5f9c74603141708cb0e0505b2a8827450230252
iotanum <evalduuk@gmail.com>
2018-10-26 11:48:45 +0300

    Initial release

---
+++ Initial_GUI.py
@@ -0,0 +1,305 @@
+import sys
+import os
+from os.path import expanduser
+from pathlib import Path
+from excel_parse import Parsing
+from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication, QMessageBox, QDesktopWidget,
+                             QFileDialog, QLabel, QVBoxLayout, QHBoxLayout, QPlainTextEdit, QLineEdit)
+from PyQt5.QtGui import QFont
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+        self.file_path = None
+        self.main_file = None
+        self.folder_path = None
+        self.results_f_path = None
+        self.main_text_input = None
+        self.secondary_text_input = None
+        self.main_file_column_status = None
+        self.if_column_found_checks = []
+
+    def initUI(self):
+        self.text = ""
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setWindowTitle('Paieška')
+        self.setGeometry(450, 450, 450, 350)
+        self.setFixedSize(self.size())
+        self.hbox = QHBoxLayout()
+        self.main_file_text_input_box()
+        self.secondary_files_text_input_box()
+        self.hbox.addStretch(1)
+        self.file_button()
+        self.folder_button()
+        self.where_to_put_button()
+        self.hbox.addWidget(self.exit_button())
+        self.hbox.addWidget(self.reset_button())
+        self.hbox.addWidget(self.start_button())
+        self.hbox.addWidget(self.export_button())
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(self.hbox)
+        self.text_field()
+        self.setLayout(vbox)
+        self.show()
+
+    def start_button(self):
+        self.start_btn = QPushButton("Pradėti paiešką")
+        self.start_btn.clicked.connect(self.start_read)
+        self.start_btn.setEnabled(False)
+        return self.start_btn
+
+    def exit_button(self):
+        cancel_button = QPushButton("Atšaukti/Išeiti")
+        cancel_button.clicked.connect(QApplication.instance().closeAllWindows)
+        return cancel_button
+
+    def reset_button(self):
+        reset_button = QPushButton("Iš naujo")
+        reset_button.clicked.connect(self.reset_UI)
+        return reset_button
+
+    def export_button(self):
+        self.export_btn = QPushButton("Ieškoti atitikimų ir eksportuoti")
+        self.export_btn.hide()
+        self.export_btn.clicked.connect(self.export_start)
+        return self.export_btn
+
+    def reset_UI(self):
+        self.file_btn.setText("Pasirinkti")
+        self.folder_btn.setText("Pasirinkti")
+        self.where_to_btn.setText("Pasirinkti")
+        self.file_btn.setEnabled(True)
+        self.folder_btn.setEnabled(False)
+        self.where_to_btn.setEnabled(False)
+        self.box.setPlainText("")
+        self.text_input_one.setText("")
+        self.text_input_two.setText("")
+        self.text_input_one.setEnabled(False)
+        self.text_input_two.setEnabled(False)
+        self.file_path = None
+        self.main_file = None
+        self.folder_path = None
+        self.results_f_path = None
+        self.main_text_input = None
+        self.secondary_text_input = None
+        self.main_file_column_status = None
+        self.if_column_found_checks = []
+        self.export_btn.hide()
+        self.start_btn.show()
+        self.start_btn.setEnabled(True if self.start_btn.setEnabled(False) else False)
+        self.export_btn.setEnabled(True)
+        self.export_btn.setText("Ieškoti atitikimų ir eksportuoti")
+        Parsing.__init__()
+
+    def export_start(self):
+        Parsing.make_excel(self.results_f_path)
+        if Parsing.found_status:
+            self.box.appendPlainText(f"Sėkmingai eksportuoti rezultatai, "
+                                     f"dokumento pavadinimas -\n"
+                                     f"'{Parsing.result_file_name}',"
+                                     f" aplanke '{os.path.basename(self.results_f_path)}'.")
+            self.export_btn.setText("Eksportuota")
+        else:
+            self.box.appendPlainText(f"Nei vieno įrašo nerasta.")
+            self.export_btn.setText("Nerasta")
+        self.export_btn.setEnabled(False)
+        self.box.verticalScrollBar().setValue(self.box.verticalScrollBar().maximum())
+
+    def start_read(self):
+        if self.checks():
+            file_paths = Parsing.get_excel_list(self.folder_path)
+            if "~$" not in str(file_paths):
+                if "" is not self.text_input_one.text() and "" is not self.text_input_two.text():
+                    self.find_and_parse_main_excel_file(self.file_path)
+                    self.find_and_parse_secondary_excel_files(file_paths)
+                    self.start_btn.hide()
+                    self.export_btn.show()
+                    self.hbox.update()
+                else:
+                    QMessageBox.about(self, "Kažkas netaip", f"Pateikite stulpelio pavadinimą!")
+            else:
+                QMessageBox.about(self, "Kažkas netaip", f"Kažkuris dokumentas yra atidarytas!")
+
+    def checks(self):
+        checks = [self.file_path, self.folder_path, self.results_f_path]
+        if not all(checks):
+            QMessageBox.about(self, "Kažkas netaip", f"Visi laukai turi būti parinkti!")
+        else:
+            return True
+
+    def find_and_parse_secondary_excel_files(self, directories):
+        counter = 1
+        if directories:
+            input_column_name = self.input_column_names_parse(self.text_input_two.text())
+            self.box.appendPlainText(f"Aplanke '{os.path.basename(self.folder_path)}' rasti šie dokumentai"
+                                     f" ir jų stulpeliai:")
+            for file_path in directories:
+                if str(self.main_file) not in str(file_path) and "rezultatai" not in str(file_path):
+                    file = str(file_path).split("\\")[-1]
+                    text = Parsing.find_needed_column(Path(file_path), input_column_name)
+                    if text is not None:
+                        self.box.appendPlainText(f"{counter}. {file}, {text}")
+                        self.if_column_found_checks.append(True)
+                    else:
+                        self.box.appendPlainText(f"{counter}. {file},\n     Reikalingas stulpelis nerastas.\n")
+                        self.if_column_found_checks.append(False)
+                    counter += 1
+            self.box.verticalScrollBar().setValue(self.box.verticalScrollBar().maximum())
+            if self.main_file_column_status:
+                self.box.appendPlainText("Jeigu viskas teisingai - spauskite "
+                                         "'Ieškoti atitikimų ir eksportuoti' mygtuką.")
+                if not all(self.if_column_found_checks):
+                    column_not_found_count = self.if_column_found_checks.count(False)
+                    self.box.appendPlainText(f"{column_not_found_count} dokument"
+                                             f"{'ai' if column_not_found_count > 1 else 'as'} "
+                                             f"neturėjo ieškomo stulpelio, "
+                                             f"prašoma dar kartą patikrinti ar viską teisingai suvedėte.")
+            else:
+                self.box.appendPlainText("Klaida!\nPatikrinkite ar gerai įvedėte stuleplių pavadinimus"
+                                         "/ar gerai parinkote reikalingą pagrindinį failą.\n"
+                                         "Bandykite viską iš naujo.")
+        else:
+            self.box.appendPlainText(f"Aplanke '{os.path.basename(self.folder_path)}' nerasta excel failų.")
+            self.export_btn.setEnabled(False)
+
+    def find_and_parse_main_excel_file(self, directory):
+        input_column_name = self.input_column_names_parse(self.text_input_one.text())
+        self.box.appendPlainText(f"\nFaile '{self.main_file}' rastas šis stulpelis:")
+        text = Parsing.find_needed_column(Path(directory), input_column_name, main=True)
+        if text is not None:
+            self.box.appendPlainText(f"1. {text[6:]}")
+            self.main_file_column_status = True
+        else:
+            self.box.appendPlainText(f"1. Reikalingas stulpelis nerastas.\n")
+            self.main_file_column_status = False
+            self.export_btn.setEnabled(False)
+
+    def input_column_names_parse(self, text):
+        return text.split(" ")
+
+    def main_file_text_input_box(self):
+        self.main_text_input_label()
+        self.text_input_one = QLineEdit(self)
+        self.text_input_one.move(265, 35)
+        self.text_input_one.resize(150, 20)
+        self.text_input_one.setEnabled(False)
+
+    def main_text_input_label(self):
+        lbl1 = QLabel('Failo, pagal kurį ieškoma\nstulpelio pavadinimas:', self)
+        lbl1.move(280, 5)
+        lbl1.resize(150, 30)
+
+    def secondary_files_text_input_box(self):
+        self.secondary_text_input_label()
+        self.text_input_two = QLineEdit(self)
+        self.text_input_two.move(265, 85)
+        self.text_input_two.resize(150, 20)
+        self.text_input_two.setEnabled(False)
+
+    def secondary_text_input_label(self):
+        lbl1 = QLabel('Failo, kuriame ieškoma\nstulpelio pavadinimas:', self)
+        lbl1.move(280, 55)
+        lbl1.resize(150, 30)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.TopLeft())
+
+    def text_field(self):
+        self.box = QPlainTextEdit(self)
+        self.box.insertPlainText(self.text)
+        self.box.setReadOnly(True)
+        self.box.move(10, 115)
+        self.box.resize(430, 195)
+
+    def file_button(self):
+        lbl1 = QLabel('<b>1.</b>', self)
+        lbl1.move(10, 10)
+        lbl2 = QLabel("Pasirinkite failą:", self)
+        lbl2.move(25, 9)
+        self.file_btn = QPushButton('Pasirinkti', self)
+        self.file_btn.setToolTip('Pasirinkite failą iš kurio bus ieškoma.')
+        self.file_btn.clicked.connect(self.select_file)
+        self.file_btn.resize(self.file_btn.sizeHint())
+        self.file_btn.move(150, 10)
+
+    def folder_button(self):
+        lbl1 = QLabel('<b>2.</b>', self)
+        lbl1.move(10, 40)
+        lbl2 = QLabel('Pasirinkite aplanką:', self)
+        lbl2.move(25, 40)
+        self.folder_btn = QPushButton('Pasirinkti', self)
+        self.folder_btn.setToolTip('Pasirinkite aplanką kuriame bus ieškoma.')
+        self.folder_btn.setEnabled(False)
+        self.folder_btn.clicked.connect(self.select_folder)
+        self.folder_btn.resize(self.folder_btn.sizeHint())
+        self.folder_btn.move(150, 42)
+
+    def where_to_put_button(self):
+        lbl1 = QLabel('<b>3.</b>', self)
+        lbl1.move(10, 70)
+        lbl2 = QLabel("Pasirinkite rastų\nrezultatų aplanką:", self)
+        lbl2.move(25, 70)
+        self.where_to_btn = QPushButton('Pasirinkti', self)
+        self.where_to_btn.setToolTip('Pasirinkite aplanką kuriame bus išsaugoti rezultatai.')
+        self.where_to_btn.setEnabled(False)
+        self.where_to_btn.clicked.connect(self.select_where_to_put)
+        self.where_to_btn.resize(self.where_to_btn.sizeHint())
+        self.where_to_btn.move(150, 74)
+
+    def select_file(self):
+        dialog = QFileDialog()
+        file_path, _ = dialog.getOpenFileName(None, caption="Pasirinkite failą", directory=expanduser("~"),
+                                              filter="Excel files (*.xlsx *.xls *.xlt)")
+        if file_path:
+            self.file_path = file_path
+            file = file_path.split("/")[-1]
+            self.main_file = file
+            self.box.appendPlainText(f"Pasirinktas '{file}' kaip pagrindinis dokumentas.")
+            self.folder_btn.setEnabled(True)
+            self.file_btn.setText("Pasirinkta")
+            self.file_btn.setEnabled(False)
+            self.text_input_one.setEnabled(True)
+
+    def select_folder(self):
+        dialog = QFileDialog()
+        folder_path = dialog.getExistingDirectory(None, caption="Pasirinkite aplanką", directory=expanduser("~"))
+        if folder_path:
+            self.folder_path = folder_path
+            self.box.appendPlainText(f"Pasirinktas '{os.path.basename(folder_path)}' aplankas.")
+            self.where_to_btn.setEnabled(True)
+            self.folder_btn.setText("Pasirinkta")
+            self.folder_btn.setEnabled(False)
+            self.text_input_two.setEnabled(True)
+
+    def select_where_to_put(self):
+        dialog = QFileDialog()
+        folder_path = dialog.getExistingDirectory(None, caption="Pasirinkite aplanką", directory=expanduser("~"))
+        if folder_path:
+            self.results_f_path = folder_path
+            self.box.appendPlainText(f"Rezultatai bus talpinami '{os.path.basename(folder_path)}' aplanke.")
+            self.where_to_btn.setText("Pasirinkta")
+            self.where_to_btn.setEnabled(False)
+            self.start_btn.setEnabled(True)
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Uždarymo langas', 'Ar tikrai norite uždaryti programą?',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
---
+++ excel_parse.py
@@ -0,0 +1,130 @@
+import os
+import string
+import time
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import traceback
+
+
+class DirectoryTree:
+    def __init__(self):
+        self.main_excel_needed_column = pd.Series(dtype=np.dtype(object))
+        self.main_excel_df = pd.Series(dtype=np.dtype(object))
+        self.not_found_df = pd.Series(dtype=np.dtype(object))
+        self.duplicates = set()
+        self.not_found_list = []
+        self.found_rows_list = []
+        self.secondary_column_list = []
+        self.result_file_name = ""
+        self.input_column_name = ""
+        self.found_status = False
+        self.row_status = False
+#        self.full_secondary = None
+
+    def file_tree(self, directory):
+        paths = list()
+        for path, dirs, files in os.walk(directory):
+            paths.append(path)
+        return paths
+
+    def excel_file_names(self, directories):
+        excel_list = list()
+        for directory in directories:
+            for file in os.listdir(directory):
+                if file.endswith(".xls") or file.endswith(".xlsx") or file.endswith(".xlt"):
+                    excel_list.append(Path(directory + "/" + file))
+        return excel_list
+
+    def get_excel_list(self, parent_dir):
+        file_list = self.excel_file_names(self.file_tree(parent_dir))
+        if file_list:
+            return file_list
+
+    def find_needed_column(self, file_path, column_name, main=False):
+        xl = pd.read_excel(file_path, header=None)
+        file_name = os.path.basename(file_path)
+        self.input_column_name = " ".join(column_name)
+        for column_num in xl:
+            for column_row in xl[column_num]:
+                if all(x.lower() in str(column_row).lower() for x in column_name):
+                    df = pd.DataFrame(xl[column_num].dropna())  # specific task - delete dropna()
+                    df.columns = [self.input_column_name]
+                    if not main:
+                        series = df.assign(file=pd.Series([file_name for _ in
+                                                           range(len(df[self.input_column_name]) + 50)]))
+#                        maybe I'll need it later for specific tasks
+#                        self.full_secondary = xl
+                        self.secondary_column_list.append(series)
+                        return f"\n     Stulpelis {self.column_alphabetic_value(column_num)} - '{column_row}'.\n"
+                    else:
+                        self.main_excel_df = xl
+                        self.main_excel_needed_column = xl[column_num]
+                        return f"\n     Stulpelis {self.column_alphabetic_value(column_num)} - '{column_row}'.\n"
+
+    def column_alphabetic_value(self, column_num):
+        return string.ascii_uppercase[column_num]
+
+    def export_excel(self, directory, df, found=True):
+        self.result_file_name = f'{"rezultatai" if found else "nerasti rezultatai"} ' + \
+                                                            str(time.strftime("%Y%m%d %H%M%S")) + ".xls"
+        print(self.result_file_name)
+        df.to_excel(directory + "/" + self.result_file_name)
+
+    def make_excel(self, directory):
+        df = pd.concat(self.secondary_column_list, ignore_index=True)
+        self.compare_main_with_secondaries(directory, df)
+
+    def make_found_excel(self, directory):
+        df2 = pd.concat(self.not_found_list, ignore_index=True)
+        print(len(df2), 'not found')
+        df = pd.concat(self.found_rows_list, ignore_index=True)
+        print(len(df), 'found')
+        self.export_excel(directory, df)
+        self.export_excel(directory, df2, found=False)
+
+    def compare_main_with_secondaries(self, directory, sec_df):
+        print(len(self.main_excel_needed_column), 'full file')
+        for main_idx, column_row in enumerate(self.main_excel_needed_column):
+            column_row = str(column_row)
+            self.row_status = False
+            for sec_idx, secondary_column_row in enumerate(sec_df[self.input_column_name]):
+                secondary_column_row = str(secondary_column_row)
+                if self.row_check(column_row, secondary_column_row) and self.input_column_name_check(column_row)\
+                        and str(column_row) not in self.duplicates:
+                    which_file_row = sec_df.iloc[[sec_idx], [1]]
+                    which_file_row = which_file_row['file'][sec_idx]
+                    new_df = self.main_excel_df.iloc[[main_idx]].assign(Rasta_dokumente=which_file_row)
+                    self.duplicates.add(str(column_row))
+                    self.found_rows_list.append(new_df)
+                    self.row_status = True
+            if not self.row_status and self.input_column_name_check(column_row):
+                not_found_row = self.main_excel_df.iloc[[main_idx]]
+                self.not_found_list.append(not_found_row)
+        if self.found_rows_list:
+            self.found_status = True
+            self.make_found_excel(directory)
+
+# For future, specific task
+#    def compare_main_with_secondaries(self, directory, sec_df):
+#        for main_idx, column_row in enumerate(self.main_excel_needed_column):
+#            column_row = str(column_row)
+#            for sec_idx, secondary_column_row in enumerate(sec_df[self.input_column_name]):
+#                secondary_column_row = str(secondary_column_row)
+#                if self.row_check(column_row, secondary_column_row) and self.input_column_name_check(column_row)\
+#                        and str(secondary_column_row) != 'nan':
+#                    which_file_row = self.full_secondary.iloc[[sec_idx], [4]]
+#                    new_df = self.main_excel_df.iloc[[main_idx]].assign(Vieta=which_file_row[4][sec_idx])
+#                    self.found_rows_list.append(new_df)
+#                    self.found_status = True
+#        if self.found_rows_list:
+#            self.make_found_excel(directory)
+
+    def row_check(self, main_row: str, secondary_row: str, ):
+        return main_row.lower() in secondary_row.lower()  # either in or ==
+
+    def input_column_name_check(self, main_row: str):
+        return self.input_column_name not in main_row.lower()
+
+
+Parsing = DirectoryTree()
